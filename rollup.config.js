137ffd8e9f83e6b2136e62038fa38ce424733f2c
Daniel Imfeld <daniel@danielimfeld.com>
2020-04-16 19:13:55 +0000

    Improve linting. Add babel. Prefer to resolve modules first

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,33 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
-import autoPreprocess from 'svelte-preprocess';
+import babel from 'rollup-plugin-babel';
 
 const production = !process.env.ROLLUP_WATCH;
+
+const babelConfig = {
+    extensions: ['.js', '.mjs', '.html', '.svelte', '.ts'],
+    exclude: ['node_modules/@babel/**', 'public/**'],
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: '> 0.25%, not dead',
+            },
+        ],
+        '@babel/preset-typescript',
+    ],
+    plugins: [
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/proposal-class-properties',
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-proposal-nullish-coalescing-operator',
+        '@babel/plugin-proposal-optional-chaining',
+    ],
+};
 
 export default {
     input: 'src/main.js',
@@ -18,9 +39,7 @@ export default {
     },
     plugins: [
         svelte({
-            preprocess: autoPreprocess({
-                postcss: true,
-            }),
+            preprocess: require('./svelte.config').preprocess,
             // enable run-time checks when not in production
             dev: !production,
             // we'll extract any component CSS out into
@@ -39,11 +58,13 @@ export default {
         // consult the documentation for details:
         // https://github.com/rollup/rollup-plugin-commonjs
         resolve({
-            browser: true,
+            mainFields: ['module', 'browser', 'main'],
+            extensions: ['.mjs', '.js', '.json', '.ts', '.svelte'],
             dedupe: importee =>
                 importee === 'svelte' || importee.startsWith('svelte/'),
         }),
         commonjs(),
+        babel(babelConfig),
 
         // Watch the `public` directory and refresh the
         // browser on changes when not in production
