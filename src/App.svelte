abb9d478d9e35f2cef9485b3467fc69c085e62ef
Daniel Imfeld <daniel@imfeld.dev>
2020-07-11 19:48:11 +0000

    remove default components

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,4 @@
 <script>
-    import Nav from './components/nav.svelte';
-    import Button from './components/Button.svelte';
-    let menuItems = [
-        { link: '#', name: 'Docs' },
-        { link: '#', name: 'Home' },
-        { link: '#', name: 'Blog' },
-    ];
 </script>
 
-<Nav {menuItems} />
-<Button text="Something" />
+<p>An app</p>
